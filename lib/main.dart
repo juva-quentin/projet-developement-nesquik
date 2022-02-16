d000d9428af74e798651b6ed676559252f9bacaa
Célian Frasca <celian.fras@gmail.com>
2022-02-16 18:41:06 +0100

    Branch

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
