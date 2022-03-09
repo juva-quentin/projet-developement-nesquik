e2c619240dc17aff811a2cefbd38fc7532e8e659
Quentin <quentin.juvet@ynov.com>
2022-03-09 15:00:53 +0100

    feat: add profil overlay with basic design

--- lib/page/profilPage.dart
+++ lib/page/profilPage.dart
@@ -1,3 +1,4 @@
+// ignore: file_names
 import 'package:flutter/material.dart';
 
 class ProfilPage extends StatefulWidget {
@@ -8,6 +9,26 @@ class ProfilPage extends StatefulWidget {
 class _ProfilPageState extends State<ProfilPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Stack(alignment: Alignment.center, children: [
+      buildTopBar(),
+    ]));
   }
+
+  Widget buildTopBar() => Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height * 0.22,
+        // ignore: prefer_const_constructors
+        decoration: BoxDecoration(
+          // ignore: prefer_const_constructors
+          color: Color(0xFF72B0EA),
+          borderRadius: BorderRadius.only(
+            // ignore: prefer_const_constructors
+            bottomLeft: Radius.circular(35),
+            bottomRight: Radius.circular(35),
+            topLeft: Radius.circular(0),
+            topRight: Radius.circular(0),
+          ),
+        ),
+      );
 }
