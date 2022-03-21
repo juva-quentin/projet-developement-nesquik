19b466767d818117b01b3694524bf732416f6d03
Quentin <quentin.juvet@ynov.com>
2022-03-21 14:54:26 +0100

    feat:add fonction for add new user in database

--- lib/auth/email_auth.dart
+++ lib/auth/email_auth.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 import 'auth_util.dart';
@@ -14,5 +17,20 @@ Future<User> createAccountWithEmail(
     BuildContext context, String email, String password) async {
   final createAccountFunc = () => FirebaseAuth.instance
       .createUserWithEmailAndPassword(email: email.trim(), password: password);
+
   return signInOrCreateAccount(context, createAccountFunc);
+}
+
+Future<void> addNewUsertoDataBase(
+    User user, String email, String pseudo, String genre) async {
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
+  users
+      .doc(user.uid)
+      .set({
+        "pseudo": pseudo,
+        "email": email,
+        "genre": genre,
+      })
+      .then((value) => print("User Added"))
+      .catchError((error) => print("Failed to add user: $error"));
 }
