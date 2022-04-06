34d6776abba7ae8b5e1818d44f9c58649d7080b6
Quentin <quentin.juvet@ynov.com>
2022-04-06 22:24:02 +0200

    fix:fix develop ultra imp

--- lib/page/Params.dart
+++ lib/page/Params.dart
@@ -68,25 +68,6 @@ class _UserInformationState extends State<UserInformation> {
   }
 
   Widget build(BuildContext context) {
-
-    return StreamBuilder<QuerySnapshot>(
-      // stream: _usersStream,
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError) {
-          return Text('Something went wrong');
-        }
-
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Text("Loading");
-        }
-
-        return ListView(
-          children: snapshot.data.docs.map((DocumentSnapshot document) {
-            Map<String, dynamic> data = document.data() as Map<String, dynamic>;
-            return ListTile();
-          }).toList(),
-        );
-
     return FutureBuilder<DocumentSnapshot>(
       future: FirebaseFirestore.instance
           .collection('users')
@@ -175,7 +156,6 @@ class _UserInformationState extends State<UserInformation> {
                 ),
               )
             : Container();
-
       },
     );
 
