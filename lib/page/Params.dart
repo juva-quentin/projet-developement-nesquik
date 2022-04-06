f856f3a380f3e1fd26a2527eeb1c316cb0799ab1
Quentin <quentin.juvet@ynov.com>
2022-04-06 21:41:54 +0200

    feat:get user info

--- lib/page/Params.dart
+++ lib/page/Params.dart
@@ -2,6 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:projet_developement_nesquik/auth/auth_util.dart';
 
+import '../auth/firebase_user_provider.dart';
+
 class Params extends StatefulWidget {
   const Params({Key key}) : super(key: key);
 
@@ -10,95 +12,28 @@ class Params extends StatefulWidget {
 }
 
 class _ParamsState extends State<Params> {
-  bool showPassword = false;
-  Stream documentStream = FirebaseFirestore.instance
-      .collection('users')
-      .doc(currentUserUid)
-      .snapshots();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFF72B0EA),
-        title: Text("Paramètres"),
-      ),
-      body: Container(
-        padding: EdgeInsets.only(left: 16, top: 25, right: 16),
-        child: GestureDetector(
-          onTap: () {
-            FocusScope.of(context).unfocus();
-          },
-          child: ListView(
-            children: [
-              SizedBox(
-                height: 15,
-              ),
-              Center(
-                child: Stack(
-                  children: [
-                    Container(),
-                    Positioned(
-                        bottom: 0,
-                        right: 0,
-                        child: Container(
-                          height: 40,
-                          width: 40,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(
-                              width: 4,
-                              color: Theme.of(context).scaffoldBackgroundColor,
-                            ),
-                            color: Color(0xFF72B0EA),
-                          ),
-                          child: Icon(
-                            Icons.edit,
-                            color: Colors.white,
-                          ),
-                        )),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 35,
-              ),
-              buildTextField("Full Name", "Evan kutto", false),
-              buildTextField("E-mail", "evan@gmail.com", false),
-              buildTextField("Password", "********", true),
-              SizedBox(
-                height: 35,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(),
-                    child: Text("CANCEL",
-                        style: TextStyle(
-                            fontSize: 19,
-                            letterSpacing: 2.2,
-                            color: Color.fromARGB(255, 255, 255, 255))),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text(
-                      "SAVE",
-                      style: TextStyle(
-                          fontSize: 19,
-                          letterSpacing: 2.2,
-                          color: Colors.white),
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
+        appBar: AppBar(
+          backgroundColor: Color(0xFF72B0EA),
+          title: Text("Paramètres"),
         ),
-      ),
-    );
+        body: UserInformation());
   }
+}
 
+class UserInformation extends StatefulWidget {
+  @override
+  _UserInformationState createState() => _UserInformationState();
+}
+
+class _UserInformationState extends State<UserInformation> {
+  bool showPassword = false;
+  final Stream<QuerySnapshot<Map<String, dynamic>>> _usersStream =
+      FirebaseFirestore.instance.collection('users').snapshots();
+
+  @override
   Widget buildTextField(
       String labelText, String placeholder, bool isPasswordTextField) {
     return Padding(
@@ -131,40 +66,99 @@ class _ParamsState extends State<Params> {
       ),
     );
   }
-}
 
-class UserInformation extends StatefulWidget {
-  @override
-  _UserInformationState createState() => _UserInformationState();
-}
-
-class _UserInformationState extends State<UserInformation> {
-  final Stream<DocumentSnapshot<Map<String, dynamic>>> _usersStream =
-      FirebaseFirestore.instance
-          .collection('users')
-          .doc(currentUserUid)
-          .snapshots();
-
-  @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: _usersStream,
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError) {
-          return Text('Something went wrong');
-        }
-
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Text("Loading");
-        }
-
-        return ListView(
-          children: snapshot.data.docs.map((DocumentSnapshot document) {
-            Map<String, dynamic> data = document.data() as Map<String, dynamic>;
-            return ListTile();
-          }).toList(),
-        );
+    return FutureBuilder<DocumentSnapshot>(
+      future: FirebaseFirestore.instance
+          .collection('users')
+          .doc(currentUser.user.uid)
+          .get(),
+      builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.hasError)
+          return Center(
+            child: Text(snapshot.hasError.toString()),
+          );
+        return snapshot.hasData
+            ? Container(
+                padding: EdgeInsets.only(left: 16, top: 25, right: 16),
+                child: GestureDetector(
+                  onTap: () {
+                    FocusScope.of(context).unfocus();
+                  },
+                  child: ListView(
+                    children: [
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Center(
+                        child: Stack(
+                          children: [
+                            Container(),
+                            Positioned(
+                                bottom: 0,
+                                right: 0,
+                                child: Container(
+                                  height: 40,
+                                  width: 40,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    border: Border.all(
+                                      width: 4,
+                                      color: Theme.of(context)
+                                          .scaffoldBackgroundColor,
+                                    ),
+                                    color: Color(0xFF72B0EA),
+                                  ),
+                                  child: Icon(
+                                    Icons.edit,
+                                    color: Colors.white,
+                                  ),
+                                )),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 35,
+                      ),
+                      buildTextField(
+                          "Full Name", snapshot.data["pseudo"], false),
+                      buildTextField("E-mail", snapshot.data["email"], false),
+                      buildTextField("Password", "********", true),
+                      SizedBox(
+                        height: 35,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ButtonStyle(),
+                            child: Text("CANCEL",
+                                style: TextStyle(
+                                    fontSize: 19,
+                                    letterSpacing: 2.2,
+                                    color: Color.fromARGB(255, 255, 255, 255))),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: Text(
+                              "SAVE",
+                              style: TextStyle(
+                                  fontSize: 19,
+                                  letterSpacing: 2.2,
+                                  color: Colors.white),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              )
+            : Container();
       },
     );
+
+    print("bite");
   }
 }
