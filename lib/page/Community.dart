8be5c4357e9e5cc380bd5d4bbbf9e3025d0ce8bd
Quentin <quentin.juvet@ynov.com>
2022-04-08 17:54:33 +0200

    fix:ios

--- lib/page/Community.dart
+++ lib/page/Community.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:projet_developement_nesquik/auth/firebase_user_provider.dart';
