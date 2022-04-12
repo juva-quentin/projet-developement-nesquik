9a53b402876f4f1493f5bd6fd268f4d2c54a57d8
Quentin <quentin.juvet@ynov.com>
2022-04-12 12:33:25 +0200

    feat:add title parcours

--- lib/backend/Parcours.dart
+++ lib/backend/Parcours.dart
@@ -10,5 +10,26 @@ class Parcours {
   List<double> denivele = [];
   double vitesse = 0.0;
 
-  Parcours();
+  Parcours(
+      [String owner,
+      String title,
+      String address,
+      String type,
+      String description,
+      List<String> shareTo,
+      double distance,
+      int temps,
+      List<double> denivele,
+      double vitesse]) {
+    this.owner = owner;
+    this.title = title;
+    this.address = address;
+    this.type = type;
+    this.description = description;
+    this.shareTo = shareTo;
+    this.distance = distance;
+    this.temps = temps;
+    this.denivele = denivele;
+    this.vitesse = vitesse;
+  }
 }
